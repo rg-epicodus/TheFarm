a496c8d83b7316b8659c2bbfa8579f4a5b35ae90
Rich Garrick <richg341@gmail.com>
2017-08-03 12:35:18 -0700

    add failing first behavior test

--- src/test/java/models/FarmTest.java
+++ src/test/java/models/FarmTest.java
@@ -1,7 +1,15 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FarmTest {
+
+    @Test
+    public void newGame_instantiatesCorrectly_true() {
+        Farm testFarm = new Farm();
+        assertEquals(false, testFarm instanceof Farm);
+    }
 
 }
